564fc204020b444a34b3d65db9e7b47c4bdaaa64
morgan.a.rennie@gmail.com <morgan.a.rennie@gmail.com>
2024-08-22 12:34:45 +0000

    Updates to Stripe Payments

---
+++ models/staging/stg__stripe_payments.sql
@@ -0,0 +1,21 @@
+with source as (
+
+    select * from {{ source('stripe', 'payment')}}
+)
+
+, transformed as (
+
+    select 
+        id as payment_id
+        , orderid as order_id
+        , paymentmethod as payment_method
+        , status as paument_status
+        , amount/100 as payment_amount_$
+        , created as payment_date
+        ,_batched_at
+
+    from source 
+
+)
+
+select * from transformed
